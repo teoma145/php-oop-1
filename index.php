aa556fc0e228b6490044fa184721676db98909d6
teoma145 <matteodark145@yahoo.it>
2023-12-04 17:47:26 +0100

    start

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include __DIR__.'/Views/header.php';
+include __DIR__.'/Model/Movie.php';
+?>
+<section class="container">
+    <h2>movies</h2>
+    <div class="row">
+    <?php foreach($movies as $movie) 
+    $movie->printcard();
+    ?>
+</div>
+</section>
+
+
+
+<?php
+include __DIR__.'/Views/footer.php';
+?>
+
